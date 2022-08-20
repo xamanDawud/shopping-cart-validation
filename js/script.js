d692d853bdc6d3b6c4ca6d9d0bce911d01cb1f09
xamanDawud <ahmeddaudking100@gmail.com>
2022-08-20 19:25:47 +0600

    cart subtotal calc complete

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-function getInputFieldValue(isIncrease) {
-    let caseInputFiled = document.getElementById("case-input-filed");
+function getInputFieldValue(isIncrease, inputId) {
+    let caseInputFiled = document.getElementById(inputId);
     let caseInputFiledValueString = caseInputFiled.value;
     let caseInputFiledValue = parseInt(caseInputFiledValueString);
     let newCaseNumber;
@@ -13,25 +13,76 @@ function getInputFieldValue(isIncrease) {
     return newCaseNumber;
 }
 
-function setPrice(priceId, total) {
+function setPrice(isPriced, priceId, total) {
     let casePriceElement = document.getElementById(priceId);
+    let caseElemetnStringValue = casePriceElement.innerText;
+    let casePrice = parseInt(caseElemetnStringValue);
+    if (isPriced) {
+        let calculate;
+    } else {
+        let calculatee;
+    }
     casePriceElement.innerText = total;
+    return casePrice;
 }
+
+let res = "";
+let res2 = "";
+// let resTotal = res + res2;
+
 document
     .getElementById("case-plus-btn")
     .addEventListener("click", function(event) {
-        let newCaseNumber = getInputFieldValue(true);
+        let newCaseNumber = getInputFieldValue(true, "case-input-filed");
         let calculate = newCaseNumber * 59;
-        setPrice("case-price", calculate);
+        setPrice(true, "case-price", calculate);
+        let subElement = document.getElementById("subTotal");
+        let subValueString = subElement.innerText;
+        let subString = parseInt(subValueString);
+        res = calculate;
+        subString = res;
+        subElement.innerText = res + res2;
     });
 
 document
     .getElementById("case-minus-btn")
     .addEventListener("click", function(event) {
-        let newCaseNumber = getInputFieldValue(false);
-        let casePriceElement = document.getElementById("case-price");
-        let casePriceValueString = casePriceElement.innerText;
-        let casePrice = parseInt(casePriceValueString);
-        let calculate = casePrice - 59;
-        setPrice("case-price", calculate);
+        let newCaseNumber = getInputFieldValue(false, "case-input-filed");
+        let calculatee = newCaseNumber * 59;
+        calculatee - 59;
+        setPrice(false, "case-price", calculatee);
+        let subElement = document.getElementById("subTotal");
+        let subValueString = subElement.innerText;
+        let subString = parseInt(subValueString);
+        res = calculatee;
+        subString = res;
+        subElement.innerText = res + res2;
+    });
+
+document
+    .getElementById("ihone-plus-btn")
+    .addEventListener("click", function(event) {
+        let newCaseNumber = getInputFieldValue(true, "iphone-input-field");
+        let calculate = newCaseNumber * 100;
+        setPrice(true, "iphone-price", calculate);
+        let subElement = document.getElementById("subTotal");
+        let subValueString = subElement.innerText;
+        let subString = parseInt(subValueString);
+        res2 = calculate;
+        subString = res2;
+        subElement.innerText = res + res2;
+    });
+
+document
+    .getElementById("ihone-minus-btn")
+    .addEventListener("click", function(event) {
+        let newCaseNumber = getInputFieldValue(false, "iphone-input-field");
+        let calculatee = newCaseNumber * 100;
+        calculatee - 100;
+        setPrice(false, "iphone-price", calculatee);
+        let subElement = document.getElementById("subTotal");
+        let subValueString = subElement.innerText;
+        let subString = parseInt(subValueString);
+        res2 = calculatee;
+        subElement.innerText = res + res2;
     });
